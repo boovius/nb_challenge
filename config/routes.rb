6cde8bf3c3123185b59e3acbb864d90bf8c63dc2
Joshua Book <joshua.book@gmail.com>
2015-06-05 08:33:13 -0700

    aggregate counts per kind for datetime range

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post '/event', to: 'events#create'
   get '/events', to: 'events#index', format: 'json'
+  get '/summary', to:'events#summary', format: 'json'
 end
